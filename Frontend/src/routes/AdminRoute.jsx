1d1f82138c22b5850aa5b33e65272f87ad29c53b
Mobin <mobintorabi592@gmail.com>
2026-07-08 15:57:42 +0330

    fix main layout

--- Frontend/src/routes/AdminRoute.jsx
+++ Frontend/src/routes/AdminRoute.jsx
@@ -2,14 +2,11 @@ import { Navigate } from "react-router";
 import { useAuth } from "../context/AuthContext";
 
 export default function AdminRoute({ children }) {
+  const { isAuthenticated, isAdmin } = useAuth();
 
-    const { isAuthenticated, isAdmin } = useAuth();
+  if (!isAuthenticated) return <Navigate to="/login" replace />;
 
-    if (!isAuthenticated)
-        return <Navigate to="/login" replace />;
+  if (!isAdmin) return <Navigate to="/tickets" replace />;
 
-    if (!isAdmin)
-        return <Navigate to="/" replace />;
-
-    return children;
+  return children;
 }
--- Frontend/src/routes/ProtectedRoute.jsx
+++ Frontend/src/routes/ProtectedRoute.jsx
@@ -2,11 +2,11 @@ import { Navigate } from "react-router";
 import { useAuth } from "../context/AuthContext";
 
 export default function ProtectedRoute({ children }) {
+  const { isAuthenticated, isAdmin } = useAuth();
 
-    const { isAuthenticated } = useAuth();
+  if (!isAuthenticated) return <Navigate to="/login" replace />;
 
-    if (!isAuthenticated)
-        return <Navigate to="/login" replace />;
+  if (isAdmin) return <Navigate to="/tickets" replace/>;
 
-    return children;
+  return children;
 }
