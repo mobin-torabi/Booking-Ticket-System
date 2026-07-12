597560c4d3872c8928949e91d20f013c4a42e5b3
lin0483 <aylinamjad@gmail.com>
2026-07-12 18:08:17 +0330

    fix

--- Frontend/src/api/httpClient.js
+++ Frontend/src/api/httpClient.js
@@ -2,11 +2,7 @@ import axios from "axios";
 
 const httpClient = axios.create({
     baseURL: import.meta.env.VITE_BACKEND_BASE_URL,
-<<<<<<< HEAD
     timeout: 100000,
-=======
-    timeout: 50000,
->>>>>>> ca85fe99604bf0c872470b33d0263ee75e74aa34
     headers: {
         "Content-Type": "application/json",
     },
